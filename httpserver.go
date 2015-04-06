329dd2d9c4d6885bea7d649e4d988e427d948436
Charles Cochrane <cochrane26@gmail.com>
2015-04-06 22:36:40 +0100

    made the clients goroutines

--- httpserver.go
+++ httpserver.go
@@ -5,7 +5,8 @@ import (
 	"github.com/boltdb/bolt"
 	"github.com/fzzy/radix/redis"
 	"github.com/julienschmidt/httprouter"
-	"github.com/sendgrid/sendgrid-go"
+	"github.com/satori/go.uuid"
+	//"github.com/sendgrid/sendgrid-go"
 	"log"
 	"net/http"
 )
@@ -14,34 +15,32 @@ import (
 // - make a alerting que
 // - add twillio support
 
-var SendgridClient *sendgrid.SGClient
-var RedisClient redis.Client
+//var RedisClient redis.Client
 var BoltClient *bolt.DB
 
-func messageHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-
-	email("cochrane26@gmail.com", "charlie", "test email", "did it work?")
+type alertSet struct {
+	UUID  string
+	score int
 }
 
-func alertHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	//give request a UUID
-	//redis("ZADD", "alerts", params.ByName("time"), UUID)
-	//boltDB(key[UUID] value[r.body])
-	// - maybe write to bolt to store the set incase of redis meltdown or just get redis to backup
-	//w.Write( UUID here )
-	//return 200
+type message struct {
+	UUID           string
+	score          int
+	recipientEmail string
+	recipientName  string
+	subject        string
+	text           string
 }
 
-func alertDeleteHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	//redis(Z delete UUID)
-	//boltDB(delete key[UUID])
-	//return 200
-}
+var RedisChannel = make(chan message)
+var SendgridChannel = make(chan message)
 
 func main() {
 	initializeClients()
 
 	go timeChecker()
+	go redisClient()
+	go sendgridClient()
 
 	router := httprouter.New()
 	router.POST("/message", messageHandler)
@@ -54,19 +53,42 @@ func main() {
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
-func email(recipientEmail string, recipientName string, subject string, text string) {
+//******************************Handlers*********************************************************
 
-	message := sendgrid.NewMail()
-	message.AddTo(recipientEmail)
-	message.AddToName(recipientName)
-	message.SetSubject(subject)
-	message.SetText(text)
-	message.SetFrom("charlie@sendgridtesting.com")
-	if r := SendgridClient.Send(message); r == nil {
-		fmt.Println("Email sent!")
-	} else {
-		fmt.Println(r)
+func messageHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	//unmarshall the JSON into message
+
+	m := message{
+		UUID:           "",
+		score:          0,
+		recipientEmail: "charlie@test.com",
+		recipientName:  "charlie",
+		subject:        "test email",
+		text:           "this is a test",
 	}
+
+	SendgridChannel <- m
+}
+
+func alertHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	uID := uuid.NewV4().String()
+	fmt.Println(uID)
+
+	m := message{UUID: uID, score: 1}
+
+	//add the UUID and score to the redis sorted set
+	RedisChannel <- m
+
+	//boltDB(key[UUID] value[r.body])
+	// - maybe write to bolt to store the set incase of redis meltdown or just get redis to backup
+	//w.Write( UUID here )
+	//return 200
+}
+
+func alertDeleteHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	//redis(Z delete UUID)
+	//boltDB(delete key[UUID])
+	//return 200
 }
 
 func timeChecker() {
@@ -82,29 +104,58 @@ func timeChecker() {
 
 //initializeClients starts the clients for Redis, BoltDB and SendGrid
 func initializeClients() {
-	//initialize Redis client
-	RedisClient, err := redis.Dial("tcp", "178.62.74.225:6379")
-	errLog(err)
-	defer RedisClient.Close()
-
-	foo, err := RedisClient.Cmd("PING").Str()
-	errLog(err)
-	log.Println("Redis Connection Reply: " + foo + " (connection accepted)")
-
-	_, err = RedisClient.Cmd("FLUSHALL").Str() //test code
-	errLog(err)                                //test code
-
 	//initialize BoltDB client
 	//this will create messages.db file in the program directory if it
 	//doesn't already exist
-	BoltClient, err = bolt.Open("messages.db", 0600, nil)
+	BoltClient, err := bolt.Open("messages.db", 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer BoltClient.Close()
 
+}
+
+func redisClient() {
+	//initialize Redis client
+	redisClient, err := redis.Dial("tcp", "pub-redis-10214.us-east-1-3.2.ec2.garantiadata.com:10214")
+	errLog(err)
+	defer redisClient.Close()
+
+	foo, err := redisClient.Cmd("PING").Str()
+	errLog(err)
+	log.Println("Redis Connection Reply: " + foo + " (connection accepted)")
+
+	_, err = redisClient.Cmd("FLUSHALL").Str() //test code
+	errLog(err)                                //test code
+
+	for {
+		m := <-RedisChannel
+		result := redisClient.Cmd("ZADD", "alerts", m.score, m.UUID)
+		errLog(result.Err)
+		fmt.Println("job Done")
+	}
+}
+
+func sendgridClient() {
+
 	//initialize Sendgrid client
-	SendgridClient = sendgrid.NewSendGridClient("charlesworth", "c30120509")
+	//sendgridClient := sendgrid.NewSendGridClient("*********", "********")
+
+	for {
+		m := <-SendgridChannel
+		fmt.Println(m.recipientEmail, m.recipientName, m.subject, m.text)
+		//	message := sendgrid.NewMail()
+		//	message.AddTo(recipientEmail)
+		//	message.AddToName(recipientName)
+		//	message.SetSubject(subject)
+		//	message.SetText(text)
+		//	message.SetFrom("charlie@sendgridtesting.com")
+		//	if r := sendgridClient.Send(message); r == nil {
+		//		fmt.Println("Email sent!")
+		//	} else {
+		//		fmt.Println(r)
+		//	}
+	}
 }
 
 func errFatal(err error) {
